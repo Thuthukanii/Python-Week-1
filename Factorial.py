22827fe0df1edd6ba3a78c6ebcff448e028b0228
Thuthukani.Langa <Thuthukani57@gmail.com>
2024-03-13 14:01:04 +0200

    Add logo

---
+++ Factorial.py
@@ -0,0 +1,12 @@
+def factorial(num):
+    if type(num) is not int:
+        return None
+    if num < 0:
+        return None
+    if num == 0:
+        return 1
+
+    return num * factorial(num - 1)
+
+
+print(factorial(4))
